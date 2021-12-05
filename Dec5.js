4e67d6edd927b6ce0e13efa5fd2745f68870086f
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2021-12-05 17:26:40 -0600

    Add files via upload

---
+++ Dec5.js
@@ -0,0 +1,12 @@
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2010;
+console.log(typeof year);
+
+console.log(typeof null);
+
